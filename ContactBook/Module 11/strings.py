b1fb2d23a8268ef762ac8ed9eb51ca8270e21f53
phutimalebana <pjrapheega@gmail.com>
2025-07-04 00:40:08 +0200

    Add files via upload

---
+++ ContactBook/Module 11/strings.py
@@ -0,0 +1,10 @@
+#Advanced concepts - Strings
+
+message = ' Hello, World! '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # Convert all characters to lowercase
+print(message.split(',')) # Split the string into a list based on the comma
+
+print(message.upper()) # Convert all characters to uppercase
+print(message.replace('World = Everyone')) # Replace the word World with Everyone  
---
+++ ContactBook/python/rectangle_calc/calculate.py
@@ -0,0 +1,8 @@
+
+def area(length, width):
+    #Function to calculate the area
+    return length * width
+
+def perimeter(length, width):
+    #Function to calculate the area
+    return 2 * (length + width)
